b1199d767c55f5e7111b8720333bb02e047c8bfb
Artem Belankov <tedtoer@gmail.com>
2017-10-15 17:42:59 +0500

    Fix comments syntax

--- 01/1.4.scm
+++ 01/1.4.scm
@@ -1,8 +1,7 @@
+; define procedure that gets 2 operands (a, b) and does:
+; (operator a b)
+; where operator defined as (if (> b 0) + -) that means
+; if b bigger 0, operator = +, else operator = -
+; then applies operator to operands (a and b)
+
 (define (a-plus-abs-b a b) ((if (> b 0) + -) a b))
-#|
-  define procedure that gets 2 operands (a, b) and does:
-  (operator a b)
-  where operator defined as (if (> b 0) + -) that means
-  if b bigger 0, operator = +, else operator = -
-  then applies operator to operands (a and b)
-#|
--- 01/1.5.scm
+++ 01/1.5.scm
@@ -1,3 +1,14 @@
+; normal-order evaluation of (test 0 (p))
+; (test 0 (p))
+; (if (= 0 0) 0 (p))
+; returns 0 because of eval operators first and
+; operators as necessary
+
+; applicative-order evaluation of (test 0 (p))
+; (test 0 (p))
+; evals infinite recursive loop of (p) because of
+; eval operands first
+
 (define (p) (p))
 (define (test x y)
   (if (= x 0)
@@ -5,16 +16,3 @@
       y))
 
 (if (= 0 0) 0 (p))
-
-#| normal-order evaluation of (test 0 (p))
-  (test 0 (p))
-  (if (= 0 0) 0 (p))
-  returns 0 because of eval operators first and
-  operators as necessary
-|#
-
-#| applicative-order evaluation of (test 0 (p))
-  (test 0 (p))
-  evals infinite recursive loop of (p) because of
-  eval operands first
-|#
