32370d4803b20de69212dcbac2ffb448fdd01d2a
Artem Belankov <tedtoer@gmail.com>
2017-10-15 17:22:14 +0500

    Solve exercise 1.5

---
+++ 01/1.5.scm
@@ -0,0 +1,20 @@
+(define (p) (p))
+(define (test x y)
+  (if (= x 0)
+      0
+      y))
+
+(if (= 0 0) 0 (p))
+
+#| normal-order evaluation of (test 0 (p))
+  (test 0 (p))
+  (if (= 0 0) 0 (p))
+  returns 0 because of eval operators first and
+  operators as necessary
+|#
+
+#| applicative-order evaluation of (test 0 (p))
+  (test 0 (p))
+  evals infinite recursive loop of (p) because of
+  eval operands first
+|#
