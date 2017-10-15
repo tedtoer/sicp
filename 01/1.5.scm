cad8ec6a7af35e15e76e6deffdc0b1597c81f48c
Artem Belankov <tedtoer@gmail.com>
2017-10-15 18:43:18 +0500

    Solve exercise 1.6, fix 1.5

--- 01/1.5.scm
+++ 01/1.5.scm
@@ -1,13 +1,13 @@
 ; normal-order evaluation of (test 0 (p))
 ; (test 0 (p))
 ; (if (= 0 0) 0 (p))
-; returns 0 because of eval operators first and
+; returns 0 because of evaluating operators first and
 ; operators as necessary
 
 ; applicative-order evaluation of (test 0 (p))
 ; (test 0 (p))
-; evals infinite recursive loop of (p) because of
-; eval operands first
+; evaluates infinite recursive loop of (p) because of
+; evaluating operands first
 
 (define (p) (p))
 (define (test x y)
---
+++ 01/1.6.scm
@@ -0,0 +1,25 @@
+(define (improve guess x)
+        (/ (+ guess (/ x guess)) 2))
+      
+(define (good_enough? guess x)
+        (< (abs (- (* guess guess) x)) 0.001))
+      
+(define (new-if predicate then-clause else-clause)
+        (cond (predicate then-clause)
+              (else else-clause)))
+
+(define (sqrt-iter guess x)
+        (new-if (good_enough? guess x)
+                guess
+                (sqrt-iter (improve guess x)
+                           x)))
+                     
+(sqrt-iter 1.0 9)
+
+; we get infinite recursive loop because of
+; elementary procedure "if" evaluates through
+; the normal-order evaluation
+;
+; and specific procedure "new-if" evaluates through
+; the applicative-order evaluation that evaluates
+; operand (sqrt-iter (improve guess x) x) first
