a4d2d088e56458b573a5dc353db49b27775ab3ac
Artem Belankov <tedtoer@gmail.com>
2017-10-15 17:00:28 +0500

    Solve exercise 1.4

---
+++ 01/1.4.scm
@@ -0,0 +1,8 @@
+(define (a-plus-abs-b a b) ((if (> b 0) + -) a b))
+#|
+  define procedure that gets 2 operands (a, b) and does:
+  (operator a b)
+  where operator defined as (if (> b 0) + -) that means
+  if b bigger 0, operator = +, else operator = -
+  then applies operator to operands (a and b)
+#|
