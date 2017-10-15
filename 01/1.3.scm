fe377eb91641847bf347dc67a7ebdeb7b50f651a
Artem Belankov <tedtoer@gmail.com>
2017-10-15 16:50:14 +0500

    Solve exercise 1.3

---
+++ 01/1.3.scm
@@ -0,0 +1,4 @@
+(define (sum-of-squares-of-two-larger a b c)
+        (cond ((and (> a b) (> c b)) (+ (* a a) (* c c)))
+              ((and (> a c) (> b c)) (+ (* a a) (* b b)))
+              (else (+ (* c c) (* b b)))))
