9f749f3105d31d69e850bb6be99aac899fb793f8
Artem Belankov <temacxz@gmail.com>
2019-09-28 21:18:22 +0300

    Add test for 01/1.3

--- solutions/01/1.3.rkt
+++ solutions/01/1.3.rkt
@@ -1,6 +1,10 @@
-#lang racket
+#lang sicp
+
+(#%require rackunit)
 
 (define (sum-of-squares-of-two-larger a b c)
         (cond [(and (> a b) (> c b)) (+ (* a a) (* c c))]
               [(and (> a c) (> b c)) (+ (* a a) (* b b))]
               [else (+ (* c c) (* b b))]))
+
+(check-equal? (sum-of-squares-of-two-larger 2 1 3) 13)
