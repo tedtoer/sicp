94143d736f8e07a177c4e5b0367bf0713c023bc2
Artem Belankov <temacxz@gmail.com>
2019-09-28 20:54:29 +0300

    Add tests for 01/1.1

--- 01/1.1.rkt
+++ 01/1.1.rkt
@@ -1,45 +1,45 @@
-#lang racket
+#lang sicp
 
-10
-; 10
+(#%require rackunit)
 
-(+ 5 3 4)
-; 12
-
-(- 9 1)
-; 8
-
-(/ 6 2)
-; 3
-
-(+ (* 2 4) (- 4 6))
-; 6
+(check-equal? (+ 5 3 4) 12)
+(check-equal? (- 9 1) 8)
+(check-equal? (/ 6 2) 3)
+(check-equal?
+  (+ (* 2 4) (- 4 6))
+  6)
 
 (define a 3)
+(check-equal? a 3)
 
 (define b (+ a 1))
+(check-equal? b 4)
 
-(+ a b (* a b))
-; 19
+(check-equal?
+  (+ a b (* a b))
+  19)
 
-(= a b)
-; #f
+(check-equal? (= a b) #f)
 
-(if (and (> b a) (< b (* a b)))
+(define cond1
+  (if (and (> b a) (< b (* a b)))
     b
-    a)
-; 4
+    a))
+(check-equal? cond1 4)
 
-(cond [(= a 4) 6]
-      [(= b 4) (+ 6 7 a)]
-      [else 25])
-; 16
+(define cond2
+  (cond [(= a 4) 6]
+        [(= b 4) (+ 6 7 a)]
+        [else 25]))
+(check-equal? cond2 16)
 
-(+ 2 (if (> b a) b a))
-; 6
+(check-equal?
+  (+ 2 (if (> b a) b a))
+  6)
 
-(* (cond [(> a b) a]
-         [(< a b) b]
-         [else -1])
-   (+ a 1))
-; 16
+(define cond3
+  (* (cond [(> a b) a]
+           [(< a b) b]
+           [else -1])
+     (+ a 1)))
+(check-equal? cond3 16)
